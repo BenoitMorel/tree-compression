f303e3b5dbef6921fe98e613e2a6ecb578ed8e5e
axel <updky@student.kit.edu>
2017-11-01 18:05:37 +0100

    added new output trees for 500 taxa

---
+++ 500/separator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat 500.nex.run1.t | sed 's/tree gen.[0-9]* = \[&U\] //g' | sed 's/ //g'| cat > test.txt
+COUNTER = 0
+while read p; do
+  COUNTER=$((COUNTER+1))
+  cat > tree_$COUNTER.nwk << EOF
+$p
+EOF
+done < test.txt
