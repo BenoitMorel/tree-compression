07a082831b8c3c96e647c0a2f8e8dce5f8ce7b71
axel <updky@student.kit.edu>
2018-05-05 10:50:51 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 static void fatal (const char * format, ...);
 
 void simpleCompressionEqual(const char * tree_file) {
-  //std::cout << "Simple compression: \n\n";
+  std::cout << "Simple compression: \n\n";
   simple_compression(tree_file, "output_files/succinct_tree.sdsl", "output_files/node_permutation.sdsl",
-                      "output_files/branch_lengths_uncompressed.sdsl", PRINT_COMPRESSION /*+ PRINT_COMPRESSION_STRUCTURES*/);
+                      "output_files/branch_lengths_uncompressed.sdsl", PRINT_COMPRESSION + PRINT_COMPRESSION_STRUCTURES);
 
   pll_utree_t * tree = pll_utree_parse_newick(tree_file);
 
@@ -25,10 +25,6 @@ void simpleCompressionEqual(const char * tree_file) {
 
   pll_unode_t * tree_loaded = simple_uncompression(succinct_tree_loaded, node_permutation_loaded, branch_lengths);
 
-  // std::cout << "---------------------------------------------------------------------\n---------------------------------------------------------------------\n";
-  // printTree(root);
-  // std::cout << "\n---------------------------------------------------------------------\n---------------------------------------------------------------------\n";
-  // printTree(tree_loaded);
   // std::cout << toNewick(root) << "\n\n\n";
   // std::cout << toNewick(tree_loaded) << "\n";
 
@@ -36,11 +32,11 @@ void simpleCompressionEqual(const char * tree_file) {
 }
 
 void RFCompressionEqual(const char * tree_file1, const char * tree_file2) {
-    //std::cout << "RF-compression: \n\n";
+    std::cout << "RF-compression: \n\n";
     rf_distance_compression(tree_file1, tree_file2, "output_files/edges_to_contract.sdsl",
                       "output_files/subtrees_succinct.sdsl", "output_files/node_permutations.sdsl",
                       "output_files/consensus_branches.sdsl", "output_files/non_consensus_branches.sdsl",
-                      PRINT_COMPRESSION /*+ PRINT_COMPRESSION_STRUCTURES*/);
+                      PRINT_COMPRESSION + PRINT_COMPRESSION_STRUCTURES);
 
     sdsl::int_vector<> edges_to_contract_loaded = uncompressRFEdgesToContract("output_files/edges_to_contract.sdsl");
     sdsl::bit_vector subtrees_succinct_loaded = uncompressSuccinctStructure("output_files/subtrees_succinct.sdsl");
@@ -54,8 +50,6 @@ void RFCompressionEqual(const char * tree_file1, const char * tree_file2) {
     setTree(root1);
     orderTree(root1);
 
-    //printTree(root1);
-
     //std::cout << "\n\n\n-------------------------RF-UNCOMPRESSION-------------------------\n\n\n";
 
     pll_unode_t * tree_rf = rf_distance_uncompression(root1, edges_to_contract_loaded, subtrees_succinct_loaded,
@@ -66,14 +60,10 @@ void RFCompressionEqual(const char * tree_file1, const char * tree_file2) {
     setTree(root2);
     orderTree(root2);
 
-    // std::cout << "Soll tree 2:\n";
-    // printTree(root2);
-    // std::cout << "Ist tree 2 RF Rekonstruktion:\n";
-    // printTree(tree_rf);
-
     // std::cout << toNewick(root2) << "\n\n\n";
     // std::cout << toNewick(tree_rf) << "\n";
 
+    assert(treesEqual(tree_rf->back, root2->back));
     std::cout << "\n" << std::boolalpha << "trees equal: " << treesEqual(tree_rf->back, root2->back) << "\n";
 
 }
@@ -97,29 +87,29 @@ int main (int argc, const char * argv[])
 
   RFCompressionEqual(argv[1], argv[2]);
 
-  // for (int i = 1; i < 1001; i++) {
-  //
-  //     for (int j = i+1; j < 1001; j+=20) {
-  //         //std::cout << "RF-compression: \n";
-  //
-  //         std::cout << i << " & " << j << ":  ";
-  //
-  //         std::stringstream ss1;
-  //         ss1 << "500\ \(500000\ gen\)/tree_" << i << ".nwk";
-  //
-  //         std::stringstream ss2;
-  //         ss2 << "500\ \(500000\ gen\)/tree_" << j << ".nwk";
-  //
-  //         //std::cout << ss1.str() << " & " << ss2.str() << "\n";
-  //
-  //         //simpleCompressionEqual(strdup(ss1.str().c_str()));
-  //         RFCompressionEqual(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
-  //         //rf_distance_compression(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
-  //     }
+  //for (int i = 1; i < 1001; i++) {
 
-
-      // //std::cout << "RF-compression: \n";
+      // for (int j = i+1; j < 1001; j+=20) {
+      //     //std::cout << "RF-compression: \n";
       //
+      //     std::cout << i << " & " << j << ":  ";
+      //
+      //     std::stringstream ss1;
+      //     ss1 << "500\ \(500000\ gen\)/tree_" << i << ".nwk";
+      //
+      //     std::stringstream ss2;
+      //     ss2 << "500\ \(500000\ gen\)/tree_" << j << ".nwk";
+      //
+      //     //std::cout << ss1.str() << " & " << ss2.str() << "\n";
+      //
+      //     //simpleCompressionEqual(strdup(ss1.str().c_str()));
+      //     RFCompressionEqual(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
+      //     //rf_distance_compression(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
+      // }
+
+
+      //std::cout << "RF-compression: \n";
+
       // std::cout << i << " & " << i+1 << ":  ";
       //
       // std::stringstream ss1;
@@ -127,12 +117,12 @@ int main (int argc, const char * argv[])
       //
       // std::stringstream ss2;
       // ss2 << "500\ \(500000\ gen\)/tree_" << (i+1) << ".nwk";
-      //
-      // //std::cout << ss1.str() << " & " << ss2.str() << "\n";
-      //
-      // simpleCompressionEqual(strdup(ss1.str().c_str()));
-      // RFCompressionEqual(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
-      // //rf_distance_compression(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
+
+      //std::cout << ss1.str() << " & " << ss2.str() << "\n";
+
+      //simpleCompressionEqual(strdup(ss1.str().c_str()));
+      //RFCompressionEqual(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
+      //rf_distance_compression(strdup(ss1.str().c_str()), strdup(ss2.str().c_str()));
   //}
 
 }
