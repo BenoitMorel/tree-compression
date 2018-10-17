89f5d420b4af3088787e1041bb6eb9ec68dd22bd
axel <updky@student.kit.edu>
2018-10-17 17:22:09 +0200

    Updated project structure

--- 027/separator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat own_example.nex.run1.t | sed 's/tree gen.[0-9]* = \[&U\] //g' | sed 's/ //g'| cat > test.txt
-COUNTER = 0
-while read p; do
-  COUNTER=$((COUNTER+1))
-  cat > tree_$COUNTER.nwk << EOF
-$p
-EOF
-done < test.txt
